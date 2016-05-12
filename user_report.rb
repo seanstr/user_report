2ddbb73b1ad6dcd55b0fc24044291572235fd4ee
Sean Matthews <seanstr@gmail.com>
2016-05-12 16:09:00 -0600

    a little cleanup and documenting

--- user_report.rb
+++ user_report.rb
@@ -28,7 +28,6 @@ qry = %{select
 
 begin
   con = Mysql.new("clientsite.com","user","pwd","db_name")
-  #rs = con.query()
   rs = con.prepare(qry)
   rs.execute "#{to_date} 23:59:59"
 
@@ -41,16 +40,19 @@ end
 
 b = []
 
+#pull data into a 2d array
 rs.each do |a|
-   h = JSON.parse(a[6]); nil
+   h = JSON.parse(a[6])
    b << [a[0], a[1], a[2], h["firstname"], h["lastname"] ||= "", a[3], a[4], a[5], h["city"], h["mycountry"], h["state"], h["league"], a[7] ||= "" ]; nil
-end;nil
+end
 
+#sort array by discountcode and lastname
 sb = b.sort_by {|e| [e[12],e[4]]}
 tb = []
 tb.push(["userid","name","username","firstname","lastname","email","registrationdate","lastvisit","city","country","state","league","discountcode"])
 sb.each {|bb| tb.push bb}
 
+#save to csv file
 File.open(filename, 'w') do |f|
    tb.each do |bb|
       f.write(CSV.generate_line(bb))
